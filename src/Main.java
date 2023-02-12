7cc52e0547b354771f47b969d9b838c34c2c3ff4
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-12 17:33:22 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double n = 64;
+
+        if (Solution.recursion(n) == 1) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution {
+    public static int recursion(double n) {
+        // Базовый случай
+        if (n == 1) {
+            return 1;
+        } // Базовый случай
+        else
+            if (n > 1 && n < 2) {
+            return 0;
+        } // Шаг рекурсии / рекурсивное условие
+        else {
+            return recursion(n / 2);
+        }
+    }
 }
